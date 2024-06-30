bb79367373504c68025b7ebe97b35987527a0665
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-06-30 11:32:10 +0600

    Update active.jsx

--- src/alls/1win/active.jsx
+++ src/alls/1win/active.jsx
@@ -5,194 +5,204 @@ import Jet from "../../assets/Lucky-Jet.jpg";
 import "./index.css";
 
 const indexactive = () => {
+setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent = 6.97 ;  
+      }
+    } , 1000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 3.72;  
+        }
+      } ,39000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 36.69;  
+          }
+        } , 66000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 59.82;  
+            }
+          } , 139000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 3.07;  
+             }
+            } , 999000)
+setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent =3.72 ;  
+              }
+            } , 1918000)
+            
+            
   setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 5.69;
-    }
-  }, 1000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1;
-    }
-  }, 34000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 6.17;
-    }
-  }, 43000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 6.67;
-    }
-  }, 80000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.86;
-    }
-  }, 118000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 3.72;
-    }
-  }, 1150000);
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 3.57;  
+              }
+            } , 1493000)
+            
+            setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent = 1.20;  
+      }
+    } , 1609000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 1.70;  
+        }
+      } ,1820000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 1.80;  
+          }
+        } , 1990000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 5.19;  
+            }
+          } , 2107000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 1.03;  
+              }
+            } , 2407000)
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.47;
-    }
-  }, 1420000);
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.31;
-    }
-  }, 1570000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 2.07;
-    }
-  }, 1710000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 7.98;
-    }
-  }, 1910000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 5.19;
-    }
-  }, 2107000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.03;
-    }
-  }, 2407000);
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1;
-    }
-  }, 2580000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 4.25;
-    }
-  }, 3800000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 2.04;
-    }
-  }, 4080000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 2.25;
-    }
-  }, 4270000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 11.38;
-    }
-  }, 4480000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 7.48;
-    }
-  }, 3600000);
 
+ setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent =1;  
+      }
+    } , 2580000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 4.25;  
+        }
+      } ,3800000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 2.04;  
+          }
+        } , 4080000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 2.25;  
+            }
+          } , 4270000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 11.38;  
+              }
+            } , 4480000)
+setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 7.48 ;  
+              }
+            } , 3600000)
+            
+            
   setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.68;
-    }
-  }, 3940000);
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 1.68;  
+              }
+            } , 3940000)
+            
+            setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent = 2.61;  
+      }
+    } , 4110000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 1.34;  
+        }
+      } ,4330000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 2.97;  
+          }
+        } , 4480000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 1.08;  
+            }
+          } , 4710000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 1.27;  
+              }
+            } , 4830000)
+            
+            
+            
+            setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 1.85;  
+              }
+            } , 4970000)
+            
+            setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent = 1;  
+      }
+    } , 5150000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 1.53;  
+        }
+      } ,5260000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 3.90;  
+          }
+        } , 5420000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 1.15;  
+            }
+          } , 5720000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 4.66;  
+              }
+            } , 5820000)
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 2.61;
-    }
-  }, 4110000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.34;
-    }
-  }, 4330000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 2.97;
-    }
-  }, 4480000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.08;
-    }
-  }, 4710000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.27;
-    }
-  }, 4830000);
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.85;
-    }
-  }, 4970000);
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1;
-    }
-  }, 5150000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.53;
-    }
-  }, 5260000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 3.9;
-    }
-  }, 5420000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.15;
-    }
-  }, 5720000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 4.66;
-    }
-  }, 5820000);
 
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.66;
-    }
-  }, 6160000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.03;
-    }
-  }, 6360000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1.04;
-    }
-  }, 6430000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 5.47;
-    }
-  }, 6610000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 1;
-    }
-  }, 3490000);
-  setTimeout(function run() {
-    if (localStorage.getItem("token")) {
-      document.getElementById("lucky").textContent = 7.48;
-    }
-  }, 3600000);
+ setTimeout(function run() {
+    if(localStorage.getItem('token')){
+        document.getElementById('lucky').textContent = 1.66 ;  
+      }
+    } , 6160000)
+    setTimeout(function run() {
+      if(localStorage.getItem('token')){
+          document.getElementById('lucky').textContent = 1.03;  
+        }
+      } ,6360000)
+      setTimeout(function run() {
+        if(localStorage.getItem('token')){
+            document.getElementById('lucky').textContent = 1.04;  
+          }
+        } , 6430000)
+        setTimeout(function run() {
+          if(localStorage.getItem('token')){
+              document.getElementById('lucky').textContent = 5.47;  
+            }
+          } , 6610000)
+          setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 1;  
+              }
+            } , 3490000)
+setTimeout(function run() {
+            if(localStorage.getItem('token')){
+                document.getElementById('lucky').textContent = 7.48 ;  
+              }
+            } , 3600000)
   const imageMap = {
     Avia: Avia,
     Jet: Jet,
